dd84019346df712016af38cb4eb015f47660be51
AlpErenSoftware <aydinalperenyazilim@gmail.com>
2026-08-24 15:43:33 +0300

    feat: add tracemalloc memory tracking to core decorator

--- benchcraft/core.py
+++ benchcraft/core.py
@@ -1,4 +1,5 @@
 import time
+import tracemalloc
 from functools import wraps
 
 def BenchCraft(unit = "s"):
@@ -7,7 +8,7 @@ def BenchCraft(unit = "s"):
 
         @wraps(func)
         def wrapper(*args, **kwargs):
-
+            tracemalloc.start()
             t_start = time.perf_counter()
 
             try:
@@ -16,11 +17,14 @@ def BenchCraft(unit = "s"):
             finally:
                 t_end = time.perf_counter()
                 elapsed_time = t_end - t_start
+                _,byte_peak = tracemalloc.get_traced_memory()
+                tracemalloc.stop()
+
 
                 display_time = elapsed_time *1000 if unit =="ms" else elapsed_time
                 unit_label = "ms" if unit =="ms" else "s"
             
-                print(f"[{func.__name__}] Run Time: {display_time:.6f} {unit_label}")
+                print(f"[{func.__name__}] Run Time: {display_time:.6f} {unit_label} | Memory: {byte_peak} B")
 
         return wrapper
     return benchcraft
